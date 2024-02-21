0443bebc8160e3ce074fd8c9afebb56cc440e906
MarinaLipenko <160345712+MarinaLipenko@users.noreply.github.com>
2024-02-21 22:17:55 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,26 @@
 package ru.netology.lipenko;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello, Marina");
+        System.out.println ("Hello, Marina");
 
-    }
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Номер транзакции: ");
+        int id = scanner.nextInt();
+
+        scanner.nextLine(); // Считываем и пропускаем символ новой строки
+
+        System.out.print("ФИО клиента: ");
+        String fio = scanner.nextLine();
+
+        System.out.print("Сумма в рублях: ");
+        double sum = scanner.nextDouble();
+
+                System.out.println("Номер транзакции: " + id + ", Клиент: " + fio + ", Сумма в рублях:  " + sum);
+
+       }
 }
