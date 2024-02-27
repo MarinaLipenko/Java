5fd237f60ceb145dc3fe2b9dd0ccb4fda843945d
MarinaLipenko <160345712+MarinaLipenko@users.noreply.github.com>
2024-02-27 22:06:54 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,26 +1,42 @@
 package ru.netology.lipenko;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println ("Hello, Marina");
+        System.out.println("Hello and welcome!");
 
+        ArrayList<Object[]> transactions = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Номер транзакции: ");
-        int id = scanner.nextInt();
+        while (true) {
+            if (transactions.size() == 5)
+                break;
+            else {
+            System.out.print("Номер транзакции: ");
+            int id = scanner.nextInt();
 
-        scanner.nextLine(); // Считываем и пропускаем символ новой строки
+            scanner.nextLine(); // Считываем и пропускаем символ новой строки
 
-        System.out.print("ФИО клиента: ");
-        String fio = scanner.nextLine();
+            System.out.print("ФИО клиента: ");
+            String fio = scanner.nextLine();
 
-        System.out.print("Сумма в рублях: ");
-        double sum = scanner.nextDouble();
+            System.out.print("Сумма в рублях: ");
+            double sum = scanner.nextDouble();
 
-                System.out.println("Номер транзакции: " + id + ", Клиент: " + fio + ", Сумма в рублях:  " + sum);
+                Object[] transactionDetails = {id, fio, sum};
+                transactions.add(transactionDetails);
+            }
+        }
 
-       }
+        // Вывод результата
+        for (Object[] transaction : transactions) {
+            for (Object detail : transaction) {
+                System.out.print(detail + " ");
+            }
+            System.out.println();
+        }
+    }
 }
